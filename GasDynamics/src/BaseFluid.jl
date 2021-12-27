60464c5408ccc22c1f61a0e6905497cb64c2bc05
Ethan Lewis Simpson <ethan.simpson@cern.ch>
2021-12-27 00:05:38 +0000

    Some additions to BaseFluid struct

--- GasDynamics/src/BaseFluid.jl
+++ GasDynamics/src/BaseFluid.jl
@@ -1,5 +1,4 @@
 
-
 module BaseFluid
 
     export Fluid,Flow,SonicVel,Mach2Vel,MakeFlow
@@ -15,16 +14,46 @@ module BaseFluid
         # The flow `class` defines the state of a fluid in terms of its thermodynamic variables and flow velocity
         FluidObj::Fluid
         MachNumber::Float64
+        Velocity::Float64
         Temperature::Float64
         Pressure::Float64
         Density::Float64
     end
 
+    function SonicVel(fluid::Fluid,T::Float64)
+        
+        """
+        Returns the acoustic velocity (speed of sound) of a particular in m/s given the temperature
+        """    
+        return (fluid.Ratio_of_Specific_Heats * fluid.Gas_Constant * T)^0.5
 
-    function MakeFlow(fluid,MachNumber;kwargs...)
+    end
+
+    function Mach2Vel(MachNumber,Temperature,flu_obj)
+
+        """ Taking MachNumber, temperature and FluidObj, computes acoustic velocity and returns velocity """
+
+        a = SonicVel(flu_obj,Temperature)
+        return a*MachNumber
+
+    end
+
+
+    function Vel2Mach(Velocity,Temperature,flu_obj)
+
+        """ Taking Velocity, temerpature and FluidObj, computes acoustic velocity and returns MachNumber """
+
+        a = SonicVel(flu_obj,Temperature)
+        return Velocity/a
+
+    end
+
+
+    function MakeFlow(fluid;kwargs...)
 
         kwargs_dict = Dict(kwargs)
         R = fluid.Gas_Constant
+
 
         if    haskey(kwargs_dict,:Temperature) && haskey(kwargs_dict,:Pressure)
             T = kwargs_dict[:Temperature]
@@ -40,28 +69,18 @@ module BaseFluid
             rho = kwargs_dict[:Density]
             p = kwargs_dict[:Pressure]
             T = p/(R*rho)
-
         end
 
-        Flow(fluid,MachNumber,T,p,rho)
 
-    end
+        if     haskey(kwargs_dict,:MachNumber) && !haskey(kwargs_dict,:Velocity)
+            MachNumber = kwargs_dict[:MachNumber]
+            Velocity   = Mach2Vel(MachNumber,T,fluid)
+        elseif haskey(kwargs_dict,:Velocity)   && !haskey(kwargs_dict,:MachNumber)
+            Velocity   = kwargs_dict[:Velocity]
+            MachNumber = Vel2Mach(Velocity,T,fluid)
+        end
 
-
-    function SonicVel(fluid::Fluid,T::Float64)
-        
-        """
-        Returns the acoustic velocity (speed of sound) of a particular in m/s given the temperature
-        """    
-        return (fluid.Ratio_of_Specific_Heats * fluid.Gas_Constant * T)^0.5
-
-    end
-
-    function Mach2Vel(flow::Flow)
-
-        a = SonicVel(flow.FluidObj,flow.Temperature)
-        println(a)
-        return a*flow.MachNumber
+        Flow(fluid,MachNumber,Velocity,T,p,rho)
 
     end
 
