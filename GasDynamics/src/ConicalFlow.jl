59f62734febe88690dcefa1416b777b94b59566c
Ethan Lewis Simpson <ethansimpson285@gmail.com>
2021-12-27 22:32:24 +0000

    Adding conical flow solver. Not sure if values are correct

---
+++ GasDynamics/src/ConicalFlow.jl
@@ -0,0 +1,63 @@
+include("./BaseFluid.jl")
+using .BaseFluid
+
+"""
+Taylor-Maccoll: What cone angle gave this shock angle?
+1. Guess a shock angle and use the oblique shock relations to guess downstream flow conditions
+2. Integrate Taylor-Maccoll equations to determine V_r, V_theta
+3. Find theta for V_theta = 0. If this matches the cone angle, found the correct conditions
+4. Try agian
+"""
+
+
+module ConicalFlow
+
+    include("./Shocks.jl")
+    using .Shocks
+
+    using PyCall
+    pushfirst!(pyimport("sys")."path","")
+    TMpython = pyimport("justTM")
+
+
+    function velocity_components(V2,theta_shock,deflec_angle)
+        V_r = V2*cos(theta_shock - deflec_angle)
+        V_theta = V2*sin(theta_shock - deflec_angle)
+        return V_r,V_theta 
+    end 
+
+
+    function generate_conical_flow_from_shock_angle(upstream,theta_shock)
+
+        """
+        Generate a conical flow field given an upstream flow and a shock angle
+        => Solves the Taylor-MacColl equations numerically to derive the flowfield behind the shockwave, 
+            from theta_shock to theta_cone, which is also compute
+        """
+        # Give theta_shock and upstream flow
+
+        # Compute behind using oblique shock
+        downstream = Shocks.ApplyObliqueShock(upstream;beta=theta_shock)
+        # Compute the deflection angle of the flow
+        deflec_angle = Shocks.ObliqueShock.deflection_angle(upstream.MachNumber,theta_shock,upstream.FluidObj.Ratio_of_Specific_Heats) # Compute here
+        #Compute the radial and transverse velocity components of the velocity field
+        V_r0 , V_theta0 = velocity_components(downstream.Velocity,theta_shock,deflec_angle)
+        # Solve the Taylor-MacColl equation numerically for the flowfield and cone angle
+        return TMpython.just_TM(theta_shock,[V_r0,V_theta0])        
+    end 
+end
+
+
+# f1 = BaseFluid.Fluid("air",1.4,287)
+
+# V1 = 5.0
+# theta_shock_deg = 35.7
+
+# flow = BaseFluid.MakeFlow(f1;MachNumber=V1,Density=1.125,Pressure=1e5)
+
+
+# a,b = ConicalFlow.generate_conical_flow_from_shock_angle(flow,deg2rad(theta_shock_deg))
+
+
+
+
---
+++ GasDynamics/src/justTM.py
@@ -0,0 +1,63 @@
+import numpy as np 
+
+from scipy.interpolate import interp1d,UnivariateSpline
+from scipy.integrate import odeint
+from mpmath import cot
+
+pi =np.pi
+
+
+def TM_fromMATLAB(g,t):
+    gamma=1.4
+    a = (gamma-1)/2
+    Numerator = a*(2*((g[0])**3-g[0])+(g[1]-g[1]*g[0]**2-g[1]**3)*cot(t)-2*g[0]*g[1]**2)-(g[0]**2*g[1]**2)
+    Denominator = a*((g[1])**2+(g[0])**2 - 1) + (g[1])**2
+    return [g[1],Numerator/Denominator]
+    # du[0] = g[1]
+    # du[1] = Numerator/Denominator
+
+
+def taylor_maccoll(u,t):
+
+    x , y = u
+
+    gamma = 1.4
+    P = (gamma-1)*0.5*(1 - x**2 - y**2)
+    du = [y,(-P*(2*x + y*cot(t)) +x*y**2)/(P - y**2)]
+    return du
+
+
+# def taylor_maccoll(y, theta, gamma=1.4):
+#     # Taylor-Maccoll function
+#     # Source: https://www.grc.nasa.gov/www/k-12/airplane/coneflow.html
+#     v_r, v_theta = y
+#     dydt = [
+#         v_theta,
+#         (v_theta ** 2 * v_r - (gamma - 1) / 2 * (1 - v_r ** 2 - v_theta ** 2) * (2 * v_r + v_theta / np.tan(theta))) / ((gamma - 1) / 2 * (1 - v_r ** 2 - v_theta ** 2) - v_theta ** 2) 
+#     ]
+#     return dydt
+
+
+def just_TM(beta_angle,y0):
+
+    # Theta range
+    thetas = np.linspace(-beta_angle, -5*pi/180, 500)
+    thetas_array = thetas.reshape((len(thetas),1))
+
+    # Define the ODE solution
+    sol = odeint(taylor_maccoll, y0, thetas)
+
+    # Define the flowfield
+    flow_field = np.concatenate((thetas_array,sol),axis=1)
+
+    # Compute the 
+    freduced = UnivariateSpline(flow_field[:,0], flow_field[:,2], s=0)
+    cone_angle = -freduced.roots()[0]
+    
+    return flow_field, cone_angle
+
+
+
+
+
+
